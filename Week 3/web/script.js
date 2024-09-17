58d6ab4ddbe13153ac0a896226fd151221d1b4a5
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-09-18 00:35:10 +0530

    improved mouse movement animation

--- Week 3/web/script.js
+++ Week 3/web/script.js
@@ -3,15 +3,10 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function circleMouse(){
-    window.addEventListener("mousemove",function(dets){
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
-    })
-}
+function  fristPageAnimation(){
 
-function firstpageani(){
     let tl =gsap.timeline();
-
+    
     tl.from("#nav" ,{
         y: -10,
         opacity:0,
@@ -34,5 +29,47 @@ function firstpageani(){
     })
 }
 
-circleMouse();
-firstpageani();
+let timeout
+function circlesquiz(){
+    // define defsult scale value
+    let xscale = 1;
+    let yscale = 1;
+    
+    let xprev = 0;
+    let yprev = 0;
+    
+    window.addEventListener("mousemove", function(dets){
+
+        clearTimeout(timeout)
+
+        let xdiff = dets.clientX - xprev;
+        xprev = dets.clientX;
+        
+        let ydiff = dets.clientY - yprev;
+        yprev = dets.clientY;
+        
+        // clamp = Clamp a value to fit within a specific range (ex: clamp(0, 100, -12) --> 0)
+        xscale = gsap.utils.clamp(0.7,1.2,xdiff)
+        yscale = gsap.utils.clamp(0.7,1.2,ydiff)
+
+        circlemousefolower(xscale,yscale);
+
+        timeout = setTimeout(()=>{
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`
+        },100)
+
+    })
+}
+
+
+function circlemousefolower(xscale,yscale){
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`
+    })
+}
+
+
+ fristPageAnimation();
+circlemousefolower();
+circlesquiz();
+
