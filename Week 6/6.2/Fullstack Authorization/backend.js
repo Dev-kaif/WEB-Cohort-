690d85b73bbb6805d88369a9fa4e0f76b4283b39
Mohammad Kaif <kaifghalib123@gmail.com>
2024-12-23 01:58:17 +0530

    made frontend and backend for authorization

---
+++ Week 6/6.2/Fullstack Authorization/backend.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+const JWT_SECRET = "WWJKCWKCWJN";
+var jwt = require("jsonwebtoken");
+
+app.use(express.json());
+app.use(cors())
+
+const users = [];
+
+app.post("/signup", function (req, res) {
+  const { username, password } = req.body;
+  const FoundUser = users.find((user) => user.username === username);
+  
+  if (FoundUser) {
+    
+    return res.status(401).json({ message: "Username already exists" });
+  }
+
+  users.push({ username: username, password: password });
+
+  res.json({ message: "User created successfully" });
+});
+
+app.post("/signin", function (req, res) {
+  const { username, password } = req.body;
+
+  const FoundUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (FoundUser) {
+    const token = jwt.sign({ username: FoundUser.username }, JWT_SECRET);
+    FoundUser.token = token;
+
+    res.json({ message: "Login successful", token: token });
+  } else {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Missing token" });
+  }
+
+  const verifiedToken = jwt.verify(token, JWT_SECRET);
+
+  const FoundUser = users.find(
+    (user) => user.username === verifiedToken.username
+  );
+
+  if (FoundUser) {
+    req.user = verifiedToken;
+    next();
+  } else {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
+}
+
+app.use(auth);
+
+app.get("/me", function (req, res) {
+  const token = req.user;
+
+  const FoundUser = users.find((user) => user.username === token.username);
+
+  res.json({ username: FoundUser.username, password: FoundUser.password });
+});
+
+app.listen(3000);
