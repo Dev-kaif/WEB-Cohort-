fae4e07fd29e65c8a439a512aa087b3ca691e96c
Mohammad Kaif <kaifghalib123@gmail.com>
2024-12-26 02:36:01 +0530

    made the user authorization middleware

---
+++ Week 8 (Backend of Course selling website)/authorization/userAuth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const {JWT_USER_SECRET} = require("../config")
+
+function auth(req,res,next){
+    const token = req.headers.token;
+
+    const response = jwt.verify(token,JWT_USER_SECRET)
+
+    if(response){
+        req.userId = response.id;
+        next()
+    }else{
+        res.status(403).json({message:"invalid Credensials"})
+    }
+}
+
+module.exports = {auth}
