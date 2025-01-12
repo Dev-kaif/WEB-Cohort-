435ef0bb6c85c13dcf873f8608397655102720f5
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-12 07:12:08 +0530

    minor style chnage in navbar

--- Week 18/18.2 (Next.js)/src/Components/navbar.tsx
+++ Week 18/18.2 (Next.js)/src/Components/navbar.tsx
@@ -1,7 +1,7 @@
 
 export default function navbar() {
   return (
-    <div className='bg-blue-600 h-10 text-white '>
+    <div className='bg-blue-600 flex items-center h-10 text-white '>
       Navbar mf
     </div>
   )
